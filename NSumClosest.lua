414275535901ddf37a577816077581c04ee27a4f
fireztron <84250829+fireztron@users.noreply.github.com>
2021-06-04 17:38:04 -0400

    Update NSumClosest.lua

--- NSumClosest.lua
+++ NSumClosest.lua
@@ -1,133 +1,181 @@
+--[[
+	For n = 1:
+		Time complexity = O[1]
+	For n = 2:
+		Time complexity = O[2]
+	For n > 2:
+		Time complexity = O[N^n]
+
+	The time complexity for n > 2 can be reduced by n - 1 for O[N^n] time complexity using pointers, however n > 2 is written with a bruteforce method and is slower than if calculated with pointers.
+]]
+
+--// Generate case
 function generateCase()
-	local arr = {}
-	local maxArrValue = 20
-	local arrcount = math.random(3, 7) + 1
-	for i = 1, arrcount do
-		table.insert(arr, math.random(maxArrValue))
-	end
-	local n = math.random(2, arrcount - 1)
-	local target = math.random(arrcount * maxArrValue)
-	return arr, n, target
+    local arr = {}
+    local maxArrValue = 20
+    local arrcount = math.random(3, 7) + 1
+    for i = 1, arrcount do
+        table.insert(arr, math.random(maxArrValue))
+    end
+    local n = math.random(2, arrcount - 1)
+    local target = math.random(arrcount * maxArrValue)
+    return arr, n, target
 end
 
-function filterIndices(arr, indices)
-	local result = {}
-	for _,indice in ipairs(indices) do
-		table.insert(result, arr[indice])
-	end
-	return result
+--// Checks if it maxed out
+local function isMaxed(indexes, index1, arrLength)
+    local max1 = indexes[#indexes]
+    local min1 = indexes[index1]
+    local n = #indexes - index1 + 1 --number of elements
+
+    return max1 - min1 + 1 == n and max1 == arrLength
 end
 
-function getSum(arr)
-	local sum = 0
-	for _,val in ipairs(arr) do
-		sum = sum + val
-	end
-	return sum
+--// Reset indexes
+local function resetIndexes(indexes, startIndexReset)
+    local res = {}
+    for i = indexes[startIndexReset], indexes[startIndexReset] + #indexes - 1 do
+        table.insert(res, i)
+    end
+    
+    return res
 end
 
-function solution(arr, n, target)
-	-- Sort the table from highest to lowest
-	table.sort(arr, function(a, b)
-		return a > b
-	end)
+--// Calculate sum
+local function getSum(arr, indexes)
+    local sum = 0
+    local res = {}
+    for position, index in ipairs(indexes) do
+        sum = sum + arr[index]
+        res[position] = arr[index]
+    end
+    return sum, res
+end
 
-	-- Select the last n items from the array and see if the sum of those surpasses target
-	local minValue = 0
-	for i = #arr, #arr - (n - 1), -1 do
-		minValue = minValue + arr[i]
-	end
-	if minValue > target then return { Success = false } end
-
-	-- Keep the current indices we want to fetch the sum for
-	-- initially we fill it with a range of 1..n
-	local currentIndices = {}
-	local currentIndicesInv = {}
-	for idx = 1, n do
-		table.insert(currentIndices, idx)
-		currentIndicesInv[idx] = #currentIndices
-	end
-
-	-- Keep repeating until we've had all possible options
-	local iters = 0
-	while true do
-		-- Calculate the sum of the given indices
-		local filteredValues = filterIndices(arr, currentIndices)
-		local sum = getSum(filteredValues)
-		-- If the sum is below the target, we return the value (future: forward lookahead)
-		if sum < target then
-			return { Result = sum, MadeWith = filteredValues, Success = true }
-		end
-				
-		-- Check if the last indice is at the end of the array
-		if currentIndices[n] == #arr then
-			-- If the last indice is at the end of the array we must shift the first
-			-- indice AFTER a free position up by 1 and reset all indices after that
-			-- in a linear pattern
-			local firstFreeIndice = nil
-			local firstIndiceAfterFreeIndice = nil
-
-			-- Check backwards if we can find an open indice spot
-			for indiceDepth = (#arr - 1), 0, -1 do
-				local indicePos = currentIndicesInv[indiceDepth]
-				if indicePos == nil then
-					firstFreeIndice = indiceDepth
-					break
-				end
-			end
-
-			-- Get the first indice before the free position
-			-- FirstFreePos is always set when we reach here since n must be lower than
-			-- #arr and n == #arr meaning there's always at least 1 free indice
-			for indiceDepth = (firstFreeIndice - 1), 0, -1 do
-				local indicePos = currentIndicesInv[indiceDepth]
-				if indicePos ~= nil then
-					firstIndiceAfterFreeIndice = indiceDepth
-					break
-				end
-			end
-			
-			-- Shift the item at firstIndiceAfterFreeIndice up by 1
-			local indicePos = currentIndicesInv[firstIndiceAfterFreeIndice]
-			currentIndices[indicePos] = firstIndiceAfterFreeIndice + 1
-			currentIndicesInv[firstIndiceAfterFreeIndice + 1] = indicePos
-			currentIndicesInv[firstIndiceAfterFreeIndice] = nil
-			
-			-- Reset all indices after firstIndiceAfterFreeIndice back to a linear pattern
-			local offset = 1
-			for i = (firstIndiceAfterFreeIndice + 1), n do
-				local invValue = currentIndices[i]
-				currentIndices[i] = i + offset
-				currentIndicesInv[i + offset] = i
-				currentIndicesInv[invValue] = nil
-				offset = offset + 1
-			end
-		else
-			-- Shift the last indice up by 1
-			local indicePos = currentIndices[n]
-			currentIndices[n] = indicePos + 1
-			currentIndicesInv[indicePos + 1] = n
-			currentIndicesInv[indicePos] = nil
-		end
-		
-		iters = iters + 1
-		if iters > 20 then
-			return { Success = false }
-		end
-	end
+local function solution(arr, n, target)
+    local foundSum = false
+    local closestSum = math.huge
+    local res = {}
+    
+    -- Sort the table from lowest to highest
+    table.sort(arr)
+    
+    -- Select the last n items from the array and see if the sum of those surpasses target
+    local minValue = 0
+    for i = 1, n, 1 do
+        minValue = minValue + arr[i]
+    end
+    if minValue > target then return { Success = false } end
+    
+    if n == 1 then
+        for _, currentNum in ipairs(arr) do
+            if math.abs(target - closestSum) > math.abs(target - currentNum) and currentNum <= target then
+                closestSum = currentNum
+                res = {currentNum}
+                foundSum = true
+            end	
+        end
+        if not foundSum then
+           return {Success = false} 
+        end
+        return {Result = closestSum, MadeWith = res, Success = true}
+    elseif n == 2 then
+        local res_l, res_r = 0,0 --result pair
+    
+        local l, r = 1, #arr
+    
+        while(r >= 1 and l <= #arr) do
+            local sum = arr[l] + arr[r]
+            if math.abs(target - closestSum) > math.abs(target - sum) and sum <= target then
+                res_l = l
+                res_r = r
+                closestSum = sum
+                foundSum = true
+            end
+            if arr[l] + arr[r] > target then
+                r = r - 1
+            else
+                l = l + 1
+            end
+        end
+        
+        res = {arr[res_l], arr[res_r]}
+        if not foundSum then
+           return {Success = false} 
+        end
+        return {Result = closestSum, MadeWith = res, Success = true}
+    elseif n > 2 then
+        --// arr does not need to be sorted
+        
+        --// Prepare indexes to check
+        local indexes = {}
+        for i = 1, n, 1 do
+            indexes[i] = i --indexes[position] = index
+        end
+        
+        --// Loop through whole array
+        local index = #indexes
+        while indexes[index] and indexes[1] <= #arr - n + 1 do
+                
+            --Check sum here and update current sum
+            local sum, tmpres = getSum(arr, indexes)
+            if math.abs(target - closestSum) > math.abs(target - sum) and sum <= target then
+                closestSum = sum
+                res = tmpres
+                foundSum = true
+            end
+    
+            --// Keep adding to index and checking sums
+            while (indexes[index + 1] == nil and indexes[index] < #arr) or (indexes[index + 1] and indexes[index + 1] ~= indexes[index] + 1) do
+    
+                --// add next index
+                indexes[index] = indexes[index] + 1
+    
+                --Check sum here and update current sum
+                local sum, tmpres = getSum(arr, indexes)
+                if math.abs(target - closestSum) > math.abs(target - sum) and sum <= target then
+                    closestSum = sum
+                    res = tmpres
+                    foundSum = true
+                end
+                
+            end
+    
+            index = index - 1
+            if index >= 1 then
+                if not isMaxed(indexes, index, #arr) then
+                    --// Go back an index
+                    indexes[index] = indexes[index] + 1
+                    indexes[index + 1] = indexes[index] + 1
+                    index = index + 1
+                else
+                    --// Reset indexes
+                    index = index - 1
+                    indexes[index] = indexes[index] + 1
+                    indexes = resetIndexes(indexes, index)
+                    index = #indexes
+                end
+            end
+        end
+        if not foundSum then
+           return {Success = false} 
+        end
+        return {Result = closestSum, MadeWith = res, Success = true}
+    end
 end
 
 local cases = {}
 for i = 1, 100 do
-	local arr1, n1, t1 = generateCase()
-	table.insert(cases, { arr = arr1, n = n1, target = t1 })
+    local arr1, n1, t1 = generateCase()
+    table.sort(arr1)
+    table.insert(cases, { arr = arr1, n = n1, target = t1 })
 end
 for _, case in ipairs(cases) do
-	print("Case: " .. table.concat(case.arr, ",") .. " n = " .. case.n .. " target = " .. case.target)
-	local sol = solution(case.arr, case.n, case.target)
-	if sol.Success then
-		print("Solution: " .. sol.Result .. " (" .. table.concat(sol.MadeWith, ",") .. ")")
-	else
-		print("No valid solution")
-	end
+    print("Case: " .. table.concat(case.arr, ",") .. " n = " .. case.n .. " target = " .. case.target)
+    local sol = solution(case.arr, case.n, case.target)
+    if sol.Success then
+        print("Solution: " .. sol.Result .. " (" .. table.concat(sol.MadeWith, ",") .. ")")
+    else
+        print("No valid solution")
+    end
 end
