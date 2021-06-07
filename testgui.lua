0d5db255ec341aa9a87d45a5f29dbcdd946bf292
fireztron <84250829+fireztron@users.noreply.github.com>
2021-06-07 05:57:23 -0400

    Update testgui.lua

--- testgui.lua
+++ testgui.lua
@@ -1,6 +1,6 @@
 --[[
     Jackpot should be working!
-        - Accurately searches for combined item prices closest to the jackpot cap (may freeze if you have many items, will solve problem in future)
+        - Accurately searches for combined item prices closest to the jackpot cap (may freeze if you have many items, will fix issue in future)
     
     Made by fireztron @ v3rmillion :)
 
