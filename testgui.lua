b96ce630ec2e4fcdacdf56e9137c53bb98b538be
fireztron <84250829+fireztron@users.noreply.github.com>
2022-04-13 22:23:01 -0400

    Update testgui.lua

--- testgui.lua
+++ testgui.lua
@@ -1,1 +1,13 @@
+warn("USE THIS: https://raw.githubusercontent.com/fireztron/Trade-Tower/main/testguiv2.lua")
 
+local function sendNotification(titleText, descText)
+    StarterGui:SetCore("SendNotification",{
+        Title = titleText;
+        Text = descText;
+        Icon = "";
+        Duration = 5;
+    })
+end
+
+sendNotification("PPHAX", "OUTDATED, USE NEW SCRIPT")
+sendNotification("USE THIS", "https://raw.githubusercontent.com/fireztron/Trade-Tower/main/testguiv2.lua")
