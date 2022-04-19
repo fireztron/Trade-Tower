51468b18a2dcf1d4a9c0631a67cbf79efce1d0d1
fireztron <84250829+fireztron@users.noreply.github.com>
2022-04-18 23:50:17 -0400

    Update testgui.lua

--- testgui.lua
+++ testgui.lua
@@ -13,4 +13,27 @@ local function sendNotification(titleText, descText)
 end
 
 sendNotification("PPHAX", "OUTDATED, USE NEW SCRIPT")
-sendNotification("USE THIS", "https://raw.githubusercontent.com/fireztron/Trade-Tower/main/testguiv2.lua")
+
+--// copy paste new script
+local function copyLoadstring()
+    setclipboard('loadstring(game:HttpGet("https://raw.githubusercontent.com/fireztron/Trade-Tower/main/testguiv2.lua"))()')
+end
+
+local StarterGui = game:GetService("StarterGui")
+local bindable = Instance.new("BindableFunction")
+
+function bindable.OnInvoke(response)
+    if response == "Yes" then
+        copyLoadstring()
+        sendNotification("loadstring copied", "have fun :D")
+    end
+end
+
+StarterGui:SetCore("SendNotification", {
+    Title = "NEW LOADSTRING",
+    Text = "Copy to clipboard?",
+    Duration = 5,
+    Callback = bindable,
+    Button1 = "Yes",
+    Button2 = "No"
+})
